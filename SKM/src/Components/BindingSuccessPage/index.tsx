79a22746bb438b92ea3c998170f3c27f7c8e4d1a
paultsai <tahsdj@gmail.com>
2023-06-12 10:49:06 +0800

    change description and add redeem button

--- SKM/src/Components/BindingSuccessPage/index.tsx
+++ SKM/src/Components/BindingSuccessPage/index.tsx
@@ -5,12 +5,23 @@ const BindingSuccessPage = () => {
   const closeWebview = useCallback(() => {
     window.liff.closeWindow();
   }, []);
+
+  const goToRedeem = useCallback(() => {
+    window.liff.openWindow({ url: "https://r.botbonnie.com/BCOK6" });
+  }, []);
   return (
     <Page type="primary">
       <div className="flex w-full flex-col items-center">
         <p className="text-white mt-[100px] mb-[200px]">
-          恭喜您已經成功完成會員綁定
+          恭喜完成新光三越會員加入，請點擊下方按鈕進行冰飲品兌換
         </p>
+
+        <button
+          className="btn btn-secondary btn-sm mt-3 mb-3"
+          onClick={goToRedeem}
+        >
+          點我兌換熱門冰飲品
+        </button>
 
         <button
           className="btn btn-secondary btn-sm mt-3 mb-5"
